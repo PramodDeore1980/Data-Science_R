b814b49bb0e6f9e3fbc00a13ee02159fca7c5c2d
PramodDeore1980 <70279191+PramodDeore1980@users.noreply.github.com>
2020-08-26 20:51:10 +0530

    Add files via upload

---
+++ First Code_25 August.R
@@ -0,0 +1,66 @@
+exp(1)
+Pi
+pi
+x <- 58
+y <- 20
+z<-100
+x*y*z
+x+y+z
+1+1
+2+3*4
+sqrt(10)
+pi
+2*pi*6378
+(6*3)+5/10 
+
+#Create vector
+x<-c(12,20,15,100)
+#Create vector
+y<-c(100,20,80,90)
+# add Vectors (x, y)
+x+y
+vector_z<- x+y
+x*5
+y*90
+# accesing the elements of the vector
+x[1]
+x[4]
+vector_z[2]
+x
+# change value in vector
+x[2]<-80
+x
+x[2]<-y[4]
+x
+# Character value in vector
+vec_chara<-c('r','pythod','java')
+vec_cha<c('R', 'Python', 'JAva',10,20)
+vect_char <- c('R','python','java',10,100.5)
+vect_PD <- c('R','Pythod',10,100.5)
+
+# Data Frame - data structure 
+
+customer<-data.frame(x=c(1,2,3),y=c("a","b","c"))
+customer[2,2]
+customer[3,3]
+customer[3,2]
+customer[1,1]
+customer[2,0]
+customer[1,1]
+customer[1,2]
+customer[1,2,2,2]
+customer[1,2]
+df[1,]
+customer[1,]
+customer[1,]
+df[1,]
+
+customer[1:1,1:2]
+df[1,]
+customer[1:2,3:3]
+customer[1,1]
+customer[1,c(1,2)]
+customer[1,]
+customer[c(1,3),2]
+customer[c(1,3),1]
+customer[c(1,3),]
